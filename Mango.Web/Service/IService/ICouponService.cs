972a0c3ed7426dc9a2d83a1b5521ef8f93e35613
Artem Gilmanov <artemapochta@gmail.com>
2023-08-23 18:30:46 +0200

    1. Changed data type for Data field in RequestDto; 2. Created Interface for CouponService; 3. Created CuponService; 4. Added field for CouponAPIBase in SD Utility; 5. Replaced content of bootstrap.css with thema: Slate from https://bootswatch.com/; 6. Added heart on the bottom of the page;

---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int couponId);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int couponId);
+
+    }
+}
