e924a88aebd7cec6b67b32f7250b8434f2f255e1
Artem Gilmanov <artemapochta@gmail.com>
2023-08-24 10:46:04 +0200

    1. Created CouponController; 2. Adjusted Layout page;

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
