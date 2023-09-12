b8d879beede648a02130476a225f85d112d6bbf6
Artem Gilmanov <artemapochta@gmail.com>
2023-09-12 20:06:06 +0200

    Set up of service bus for messages.

---
+++ Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+
+namespace Mango.MessageBus
+{
+    internal class MessageBus : IMessageBus
+    {
+        private string connectionString = "connectionStringFromServiceBus";
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConverter.SerializeObject(message);
+
+            ServiceBusMessage finaleMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await sender.SendMessageAsync(finaleMessage);
+            await client.DisposeAsync();
+
+        }
+    }
+}
